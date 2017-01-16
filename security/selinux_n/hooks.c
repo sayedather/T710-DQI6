dc27cb945d267812de1cf0402a044a98d8fc2a5f
Luca Grifo <lg@linux.com>
2017-01-16 15:37:37 +0100

    selinux: permissive patch to N source adapted for 3.10 kernel source
    Signed-off-by: Luca Grifo <lg@linux.com>
    Signed-off-by: geiti94 <geiti94@me.com>

--- security/selinux_n/hooks.c
+++ security/selinux_n/hooks.c
@@ -152,8 +152,10 @@ static int __init enforcing_setup(char *str)
 	unsigned long enforcing;
 	if (!strict_strtoul(str, 0, &enforcing))
 // [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
+#if defined(CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE)
 		selinux_enforcing = 1;
+#elif defined(CONFIG_SECURITY_SELINUX_NEVER_ENFORCE)
+		selinux_enforcing = 0;
 #else
 		selinux_enforcing = enforcing ? 1 : 0;
 #endif
@@ -171,7 +173,7 @@ static int __init selinux_enabled_setup(char *str)
 	unsigned long enabled;
 	if (!strict_strtoul(str, 0, &enabled))
 // [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
 		selinux_enabled = 1;
 #else
 		selinux_enabled = enabled ? 1 : 0;
@@ -423,6 +425,7 @@ static int may_context_mount_inode_relabel(u32 sid,
 			  FILESYSTEM__ASSOCIATE, NULL);
 	return rc;
 }
+
 
 static int sb_finish_set_opts(struct super_block *sb)
 {
@@ -5206,7 +5209,7 @@ static int selinux_nlmsg_perm(struct sock *sk, struct sk_buff *skb)
 				  " type=%hu for sclass=%hu\n",
 				  nlh->nlmsg_type, sksec->sclass);
 // [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
 			if (security_get_allow_unknown())
 #else
 			if (!selinux_enforcing || security_get_allow_unknown())
@@ -6531,7 +6534,7 @@ static __init int selinux_init(void)
 {
 	if (!security_module_enable(&selinux_ops)) {
 // [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
 		selinux_enabled = 1;
 #else
 		selinux_enabled = 0;
@@ -6560,9 +6563,12 @@ static __init int selinux_init(void)
 	if (register_security(&selinux_ops))
 		panic("SELinux: Unable to register with kernel.\n");
 // [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
-	selinux_enforcing = 1;
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
+  		selinux_enforcing = 1;
+#elif defined(CONFIG_SECURITY_SELINUX_NEVER_ENFORCE)
+		selinux_enforcing = 0;
 #endif
+
 // ] SEC_SELINUX_PORTING_COMMON
 	if (selinux_enforcing)
 		printk(KERN_DEBUG "SELinux:  Starting in enforcing mode\n");
@@ -6641,9 +6647,10 @@ static int __init selinux_nf_ip_init(void)
 {
 	int err = 0;
 // [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
-	selinux_enabled = 1;
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
+  		selinux_enabled = 1;
 #endif
+
 // ] SEC_SELINUX_PORTING_COMMON
 	if (!selinux_enabled)
 		goto out;
