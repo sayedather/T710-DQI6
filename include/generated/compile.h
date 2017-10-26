5aca3bab479ebd1017e41cb46fe407142f26b3d2
Ather <ather90@gmail.com>
2017-10-26 15:25:36 +0300

    Source-upload

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 SMP PREEMPT Wed Oct 25 09:16:36 AST 2017"
+#define LINUX_COMPILE_BY "ather"
+#define LINUX_COMPILE_HOST "ather-android"
+#define LINUX_COMPILER "gcc version 4.8 (GCC) "
